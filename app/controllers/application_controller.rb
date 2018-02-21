5b0f6743cb998c3410719135f2707880e23e75df
Wendy S <wstaub@fastmail.fm>
2018-02-21 09:24:15 -0500

    Day2 Silde 12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :categories, :brands
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def categories
+  	@categories = Category.order(:name)
+  end
+
+# Pluck grabs everything in one column - there will be multiples
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'categorical' =>'storefront#items_by_category'
 
-  get 'brinding' => 'storefront#items_by_brand'
+  get 'branding' => 'storefront#items_by_brand'
 
 # generates 7 routes for 5 product views
   resources :products
